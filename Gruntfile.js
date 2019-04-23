1cae2a34780d16eafd70dd81e1ab42862c85481e
yangmeijing <1806102644@qq.com>
2019-04-23 15:52:09 +0800

    修改Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,47 +29,29 @@ module.exports = function (grunt) {
   htmlmin: {
   options: {
           collapseWhitespace: true,
-                  preserveLineBreaks: false
+          preserveLineBreaks: false
                         
   },
   index:{
-  files: {
-        //'dist/dlg-about/index.html': './dlg-about/index.html',
-        //'dist/dlg-search/index.html': './dlg-search/index.html',
-        //'dist/dlg-font/index.html': './dlg-font/index.html',
-        //'dist/list/index.html': './list/index.html',
-        //'dist/dlg-goto/index.html': './dlg-goto/index.html',
-        //'dist/dlg-replace/index.html': './dlg-replace/index.html',
-        'dist/index.html':'./index.html',
-        'dist/':'./*/index.html'
-
-
-                        
-  }
+    files: {
+      src:['*.html','*-*/*.html','list/*.html'],
+      dest:'dist/'                        
+    }
   }    
   },
   cssmin: {
-        'dist/dlg-about/dlg-about.css': './dlg-about/dlg-about.css',
-        'dist/dlg-search/dlg-search.css': './dlg-search/dlg-search.css',
-        'dist/dlg-font/dlg-font.css': './dlg-font/dlg-font.css',
-        'dist/list/list.css': './list/list.css',
-        'dist/dlg-goto/dlg-goto.css': './dlg-goto/dlg.css',
-        'dist/dlg-replace/dlg-replace.css': './dlg-replace/dlg-replace.css',
-
-
-                        
-
+  files:{
+      expand:true,
+      src:['*-*/*.css','list/*.css'],
+      dest:'dist/'
+                                           
+  }
   },
   uglify: {
   release:{
   files: {
-        'dist/dlg-about/dlg-about.js': './dlg-about/dlg-about.js',
-        'dist/dlg-search/dlg-search.js': './dlg-search/dlg-search.js',
-        'dist/dlg-font/dlg-font.js': './dlg-font/dlg-font.js',
-        'dist/list/list.js': './list/list.js',
-        'dist/dlg-goto/dlg-goto.js': './dlg-goto/dlg.js',
-        'dist/dlg-replace/dlg-replace.js': './dlg-replace/dlg-replace.js',
-
+      src:['*-*/*.js','list/*.js'],
+      dest:'dist/'
 
   }
         
